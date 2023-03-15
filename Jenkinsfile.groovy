abc8371d07fe7e451a7ba9fac0e43705871565ee
Devopmaster <121347801+Devopmaster@users.noreply.github.com>
2023-03-15 12:11:03 +0530

    Add files via upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent {
+        label {
+            
+            label 'built-in'
+          
+                } 
+    }
+
+stages {
+        stage ('httpd') {
+            steps {
+                sh 'service httpd start'
+            }
+
+    stages {
+        stage ('clone') {
+            steps {
+                dir ('/www/main/') {
+                sh "rm -rf *"
+                sh 'git clone -b main https://github.com/Devopmaster/branchindex.git'
+                sh "cp -r /www/main/branchindex/index.html /var/www/html"
+                sh "chmod 777 /var/www/html/index.html"
+                        }
+                    }
+                }
+            }
+        }     
+    }
+}
